179ee62f4acfaaad3f9207f4ecd5fc0bc9e9e84e
cloudyan <1395093509@qq.com>
2024-03-07 22:06:28 +0800

    feat: update config-provider demo3

--- src/components/config-provider/config-provider.tsx
+++ src/components/config-provider/config-provider.tsx
@@ -49,7 +49,6 @@ export const defaultConfigRef: {
 } = {
   current: {
     locale: zhCN,
-    theme: 'light',
   },
 }
 
--- src/components/config-provider/demos/demo3.tsx
+++ src/components/config-provider/demos/demo3.tsx
@@ -7,54 +7,59 @@ import {
   Slider,
   Space,
   Switch,
+  useConfig,
 } from 'antd-mobile'
 import { DemoBlock } from 'demos'
 import zhCN from 'antd-mobile/es/locales/zh-CN'
 import enUS from 'antd-mobile/es/locales/en-US'
+import { useUpdateLayoutEffect } from 'ahooks'
 import './demo3.less'
 
 export default () => {
   const [rateValue, setRateValue] = useState(3)
   const [sliderValue, setSliderValue] = useState(60)
   const [enableDarkMode, setEnableDarkMode] = useState(true)
+  const [language, setLanguage] = useState('zh-cn')
+  const { locale } = useConfig()
 
   useLayoutEffect(() => {
-    document.documentElement.setAttribute(
-      'data-prefers-color-scheme',
-      enableDarkMode ? 'dark' : 'light'
-    )
+    const mode = enableDarkMode ? 'dark' : 'light'
+    document.documentElement.setAttribute('data-prefers-color-scheme', mode)
   }, [enableDarkMode])
 
-  function toLight() {
-    setDefaultConfig({
-      locale: zhCN,
-      theme: 'light',
-    })
+  const updateMode = (v: any) => {
+    setEnableDarkMode(v)
+    const mode = v ? 'dark' : 'light'
     Dialog.alert({
-      content: '已切换到 Light theme',
+      content: `已切换到 ${mode} mode`,
     })
   }
-  function toDark() {
-    setDefaultConfig({
-      locale: enUS,
-      theme: 'dark',
-    })
+
+  useUpdateLayoutEffect(() => {
+    const content = language === 'en' ? 'Switch to English' : '已切换到 中文'
     Dialog.alert({
-      content: 'Switched to Dark theme',
+      title: locale.common.confirm,
+      content: `${content} ${language}`,
+    })
+  }, [language])
+
+  const updateLanguage = (v: any) => {
+    const lang = language !== 'en' ? 'en' : 'zh-cn'
+    setLanguage(lang)
+    setDefaultConfig({
+      locale: language !== 'en' ? enUS : zhCN,
     })
   }
+
   return (
     <>
-      <DemoBlock title='Switch'>
-        <Space align='center'>
-          <div>Dark Mode</div>
-          <Switch
-            checked={enableDarkMode}
-            onChange={v => {
-              setEnableDarkMode(v)
-            }}
-          />
-        </Space>
+      <DemoBlock title='切换语言'>
+        <Switch checked={language === 'en'} onChange={updateLanguage} />{' '}
+        {language}
+      </DemoBlock>
+      <DemoBlock title='切换明暗模式'>
+        <Switch checked={enableDarkMode} onChange={updateMode} />{' '}
+        {enableDarkMode ? 'dark' : 'light'}
       </DemoBlock>
 
       <DemoBlock title='默认主题'>
